66ffd8d0ba9b2b84caaee48719a7327794ab1d26
Syed Waqas <waqas.shah.syed@gmail.com>
2018-01-21 12:35:31 +0500

    Ninject module for Identity application layer

---
+++ src/ForumApp/IdentityAndAccess/Application/ForumApp.Identity.Application/Ninject/IdentityApplicationNinjectModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForumApp.Identity.Application.ApplicationServices;
+using ForumApp.Identity.Infrastructure.Persistence.Repositories;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace ForumApp.Identity.Application.Ninject
+{
+    /// <summary>
+    /// Ninject Module
+    /// </summary>
+    public class IdentityApplicationNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IAccountApplicationService>().To<AccountApplicationService>().InRequestScope();
+        }
+    }
+}

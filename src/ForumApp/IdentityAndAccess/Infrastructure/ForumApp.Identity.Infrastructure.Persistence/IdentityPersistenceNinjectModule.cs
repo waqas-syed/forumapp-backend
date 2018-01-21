c7fa3054f25163c69a1a1edb448c5a13960f3f1f
Syed Waqas <waqas.shah.syed@gmail.com>
2018-01-21 11:55:50 +0500

    Adding Ninject Module for Identity's Persistence layer

---
+++ src/ForumApp/IdentityAndAccess/Infrastructure/ForumApp.Identity.Infrastructure.Persistence/IdentityPersistenceNinjectModule.cs
@@ -0,0 +1,22 @@
+﻿using ForumApp.Identity.Infrastructure.Persistence.PersistenceBase;
+using ForumApp.Identity.Infrastructure.Persistence.Repositories;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace ForumApp.Identity.Infrastructure.Persistence
+{
+    /// <summary>
+    /// Ninject module for Identity's Persistence layer
+    /// </summary>
+    public class IdentityPersistenceNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IdentityAndAccessContext>().ToSelf().InRequestScope();
+            Bind(typeof(UserManager<IdentityUser>)).ToSelf().InRequestScope();
+            Bind<IAccountRepository>().To<AccountRepository>().InRequestScope();
+        }
+    }
+}

440ee86cc12570b43789a320e0e586b53c12349d
Syed Waqas <waqas.shah.syed@gmail.com>
2018-01-23 19:28:35 +0500

    AddCommentCommand added again after accidental deletion

---
+++ src/ForumApp/Forum/Application/ForumApp.Forum.Application/ApplicationServices/Commands/AddCommentCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForumApp.Forum.Application.ApplicationServices.Commands
+{
+    [Serializable]
+    public class AddCommentCommand
+    {
+        public string PostId { get; set; }
+        public string AuthorEmail { get; set; }
+        public string Text { get; set; }
+    }
+}
